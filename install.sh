7374da0cd2e54c7f3d4dbc8782309670124262f9
Albrecht Naumann <albrecht.naumann@zalari.de>
2022-08-01 20:59:04 +0200

    current config

--- install.sh
+++ install.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 BASEDIR=$(pwd)
 
+if ! [ -d ~/.oh-my-zsh ]; then
+  git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+fi
+
 ZSHRC=$HOME/.zshrc
-rm $ZSHRC
+rm $ZSHRC || true
 ln -s $BASEDIR/zsh/.zshrc $ZSHRC
+
+TMUX=$HOME/.tmux.conf
+rm $TMUX || true
+ln -s $BASEDIR/tmux/.tmux.conf $TMUX
+
+
+packages=(
+  "tmux"
+  "fzf" 
+  "reattach-to-user-namespace"
+)
+
+for lib in "${packages[@]}"; do
+  echo "check for $lib"
+  brew ls --versions $lib || brew install $lib
+done
+
+if ! [ -d ~/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
